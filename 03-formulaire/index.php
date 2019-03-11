6a0ad9294b4e474d9f6a81eac07b570fa59d8aa4
Matthieu Mota <matthieumota@gmail.com>
2019-03-11 16:14:41 +0100

    Fix exo form

--- 03-formulaire/index.php
+++ 03-formulaire/index.php
@@ -1,20 +1,47 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
     <!--
-        Formulaire avec AJAX:
+        Formulaire:
         1. Ajouter Bootstrap sur la page.
         2. Ajouter un formulaire en POST avec deux champs (Nom et message).
         3. Le formulaire sera traité sur le fichier worker.php (action).
         4. On va vérifier que le nom et le message fasse au moins 2 caractères.
         5. Si le formulaire est valide, on affiche "Succès".
         6. S'il y a des erreurs, on les affiche.
+        7. AJAX en BONUS
     -->
-</body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-6 offset-3">
+                <form action="./worker.php" method="post">
+                    <div class="form-group">
+                        <label for="name">Nom :</label>
+                        <input type="text" name="name" id="name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message :</label>
+                        <input type="text" name="message" id="message" class="form-control">
+                    </div>
+                    <button class="btn btn-primary btn-block">Envoyer</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </body>
 </html>
---
+++ 03-formulaire/worker.php
@@ -0,0 +1,30 @@
+<?php
+
+// On vérifie que le formulaire a été soumis
+if ('POST' === $_SERVER['REQUEST_METHOD']) {
+    // On récupère les champs du formulaire
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+
+    // On prépare le tableau avec les erreurs
+    $errors = [];
+
+    // On vérifie le champ name
+    if (strlen($name) < 2) {
+        $errors['name'] = 'Erreur name';
+        // echo 'Erreur name';
+    }
+
+    // On vérifie le champ message
+    if (strlen($message) < 2) {
+        $errors['message'] = 'Erreur message';
+        // echo 'Erreur message';
+    }
+
+    // On vérifie si le formulaire contient des erreurs
+    if (empty($errors)) {
+        echo 'Succès';
+    } else {
+        print_r($errors);
+    }
+}

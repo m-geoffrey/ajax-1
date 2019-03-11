61336bdbb85457b915547a1233357b5f67fc7295
Matthieu Mota <matthieumota@gmail.com>
2019-03-11 15:18:10 +0100

    Form ajax

---
+++ 03-formulaire/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <!--
+        Formulaire avec AJAX:
+        1. Ajouter Bootstrap sur la page.
+        2. Ajouter un formulaire en POST avec deux champs (Nom et message).
+        3. Le formulaire sera traité sur le fichier worker.php (action).
+        4. On va vérifier que le nom et le message fasse au moins 2 caractères.
+        5. Si le formulaire est valide, on affiche "Succès".
+        6. S'il y a des erreurs, on les affiche.
+    -->
+</body>
+</html>

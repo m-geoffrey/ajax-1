33f10f64fccd7dd7c34f1acf88c8f5c1a4dabfe1
Matthieu Mota <matthieumota@gmail.com>
2019-03-12 12:27:24 +0100

    Display smartphones as json

--- 04-api/smartphone.php
+++ 04-api/smartphone.php
@@ -5,5 +5,11 @@ $db = new PDO('mysql:host=mysql.docker;dbname=smartphone;charset=utf8', 'root', 
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
-// On récupère les smartphones
+// On récupère les smartphones en associatif
+$sql = 'SELECT * FROM smartphone';
+$query = $db->query($sql);
+$smartphones = $query->fetchAll(PDO::FETCH_ASSOC);
 
+// Transformer le tableau en JSON (json_encode)
+header('Content-Type: application/json');
+echo json_encode($smartphones);
